9a9805ee284250a4fcac3de8c542afdf1f75c69e
hoxx <haru9760@naver.com>
2021-06-03 14:27:24 +0900

    feat: slack url 수정

--- src/main/java/com/js/ms/todo/global/config/slack/SlackChannel.java
+++ src/main/java/com/js/ms/todo/global/config/slack/SlackChannel.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 @Getter
 public enum SlackChannel {
 
-    CHANNEL_EXCEPTION("https://hooks.slack.com/services/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"),
-    CHANNEL_MEMBER("https://hooks.slack.com/services/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");
+    CHANNEL_EXCEPTION("https://hooks.slack.com/services/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"),
+    CHANNEL_MEMBER("https://hooks.slack.com/services/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");
 
     private String url;
 
