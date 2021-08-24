83b72cd5db0a3668e594c2a1576b1459793efb91
hoxx <haru9760@naver.com>
2021-08-24 15:49:07 +0900

    feat: 젠킨스 CI,CD 테스트

--- src/main/java/com/js/ms/todo/TodoApplication.java
+++ src/main/java/com/js/ms/todo/TodoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
-        System.out.println("테스트1");
+        System.out.println("테스트2");
         SpringApplication.run(TodoApplication.class, args);
     }
 
