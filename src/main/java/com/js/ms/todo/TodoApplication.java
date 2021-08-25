5fdd993bdd7dc3c9116697cd7de6bd6619561141
hoxx <haru9760@naver.com>
2021-08-25 16:05:13 +0900

    feat: 젠킨스 CI,CD 테스트 및 db 연결 테스트

--- src/main/java/com/js/ms/todo/TodoApplication.java
+++ src/main/java/com/js/ms/todo/TodoApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
-        System.out.println("테스트8");
         SpringApplication.run(TodoApplication.class, args);
     }
 
